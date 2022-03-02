a01d01c503987c29e4b7a1562dc7ed4154c048c8
“coadaro” <coadaro@gmail.com>
2022-03-02 17:33:10 -0500

    LiteralMathMethodExerciseWithBonus

---
+++ script.js
@@ -0,0 +1,30 @@
+//1
+let warmHugs="Hi, I'm Olaf and I like warm hugs.";
+console.log(warmHugs.toUpperCase());
+console.log(warmHugs.replace("like", "love"));
+//2
+let beenImpaled="Oh, look at that. I've been impaled.";
+console.log(beenImpaled.slice(18));
+//3
+let dotDotDot="...";
+let skullBones=`I don't have a skull${dotDotDot}or bones`
+console.log(skullBones);
+//4
+console.log(Math.PI);
+//5
+let randomNumber=Math.random();
+randomNumber*=3;
+randomNumber=Math.ceil(randomNumber)
+console.log(randomNumber);
+
+//bonus
+//6
+let letItGo=" Let It Go!";
+console.log(letItGo.toUpperCase().repeat(2));
+//7
+let reindeers="Reindeers are better than people."
+
+//8
+console.log(Math.sqrt(2));
+//9
+console.log(Math.floor(Math.random() * 23) + 7);
