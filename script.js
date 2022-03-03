210b74a8fa39e46de906daa0ac0823b6d8d8ce93
“coadaro” <coadaro@gmail.com>
2022-03-03 10:59:19 -0500

    UpdatedBonus

--- script.js
+++ script.js
@@ -1,30 +1,45 @@
 //1
 let warmHugs="Hi, I'm Olaf and I like warm hugs.";
 console.log(warmHugs.toUpperCase());
-console.log(warmHugs.replace("like", "love"));
+warmHugs=warmHugs.replace("like", "love");
+//or to check
+// console.log(warmHugs.replace("like", "love"));
+
 //2
 let beenImpaled="Oh, look at that. I've been impaled.";
-console.log(beenImpaled.slice(18));
+beenImpaled=beenImpaled.slice(18);
+//or to check
+// console.log(beenImpaled.slice(18));
+
 //3
 let dotDotDot="...";
-let skullBones=`I don't have a skull${dotDotDot}or bones`
+let skullBones=`I don't have a skull${dotDotDot}or bones`;
 console.log(skullBones);
+
 //4
 console.log(Math.PI);
+
 //5
 let randomNumber=Math.random();
 randomNumber*=3;
-randomNumber=Math.ceil(randomNumber)
+randomNumber=Math.floor(randomNumber);
+randomNumber++;
 console.log(randomNumber);
+//using .ceil and no ++ has a small chance to land on 0.0
 
 //bonus
 //6
-let letItGo=" Let It Go!";
-console.log(letItGo.toUpperCase().repeat(2));
+console.log(" Let It Go!".toUpperCase().repeat(2).trim());
+
 //7
-let reindeers="Reindeers are better than people."
+let reindeers="Reindeers are better than people.";
+console.log(reindeers.replaceAll(' ', '_'));
+//or
+// console.log(reindeers.replace(/ /g, '_'));
 
 //8
 console.log(Math.sqrt(2));
+
 //9
-console.log(Math.floor(Math.random() * 23) + 7);
+let newRandomNumber=(Math.floor(Math.random() * 17) + 7);
+console.log(newRandomNumber)
